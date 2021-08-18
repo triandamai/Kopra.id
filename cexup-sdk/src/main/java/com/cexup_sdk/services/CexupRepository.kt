6fe21ca46d3838a7da8099aaf64a52d4bd5ed502
triandamai <triannurizkillah@gmail.com>
2021-08-18 12:32:04 +0700

    feat: update testing

--- cexup-sdk/src/main/java/com/cexup_sdk/services/CexupRepository.kt
+++ cexup-sdk/src/main/java/com/cexup_sdk/services/CexupRepository.kt
@@ -1,6 +1,6 @@
 package com.cexup_sdk.services
 
-class CexupRepository(private val cexupApi: CexupApi):Repository {
+class CexupRepository(private val cexupApi: Api):Repository {
     override suspend fun login() {
 
     }
