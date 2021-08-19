1f31d2571a91870f4ad4a0ead3365fa3a7999d08
triandamai <triannurizkillah@gmail.com>
2021-08-19 14:09:22 +0700

    feat: update color pallete

---
+++ cexup-features/src/main/java/com/cexup/features/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.cexup.features.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val bgDark = Color(0xFF141C3D)
+val bgAccentDark = Color(0xFF202D5B)
+
+val bgLight = Color(0xFFF9FBFC)
+val bgAccentLight = Color(0xFFFFFFFF)
+
+val bgBlueDarken = Color(0xFF3FA7EF)
+val bgBlueLighten = Color(0xFF3DB4FC)
+
+val bgPurpleDarken = Color(0xFF906AF7)
+val bgPurpleLighten= Color(0xFF6188FB)
+val bgPurpleLighten1= Color(0xFFBF2EFF)
+
+val bgRedDarken = Color(0xFFF14F72)
+val bgRedLighten = Color(0xFFFF465F)
+
+val bgOrangeDarken = Color(0xFFFE724C)
+val bgOrangeLighten = Color(0xFFFC9E62)
+
+val bgGreenDarken = Color(0xFF20B574)
+val bgGreenLighten = Color(0xFF26ED83)
+
+val bgAccent = Color(0xFFC4C4C4)
