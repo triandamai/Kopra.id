cd4322903d45685dd7e5e60d6e76a5cba1208d65
triandamai <triannurizkillah@gmail.com>
2021-08-27 17:53:10 +0700

    feat: add more components

---
+++ features/bmi/src/main/java/com/trian/bmi/BmiActivity.kt
@@ -0,0 +1,38 @@
+package com.trian.bmi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.trian.bmi.ui.theme.TesMultiModuleTheme
+
+class BmiActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TesMultiModuleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    TesMultiModuleTheme {
+        Greeting2("Android")
+    }
+}
---
+++ features/camera/src/main/java/com/trian/camera/CameraActivity.kt
@@ -0,0 +1,38 @@
+package com.trian.camera
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.trian.camera.ui.theme.TesMultiModuleTheme
+
+class CameraActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TesMultiModuleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting3("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    TesMultiModuleTheme {
+        Greeting3("Android")
+    }
+}
