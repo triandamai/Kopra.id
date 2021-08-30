7b1289deea529dcfcd3372ecb4f9de8fa2161d02
triandamai <triannurizkillah@gmail.com>
2021-08-30 10:37:08 +0700

    feat: finishing data module
    #133

--- data/src/main/java/com/trian/data/remote/app/AppApiServices.kt
+++ data/src/main/java/com/trian/data/remote/app/AppApiServices.kt
@@ -3,15 +3,59 @@ package com.trian.data.remote.app
 import com.trian.domain.repository.BaseResponse
 import com.trian.domain.entities.Nurse
 import com.trian.domain.entities.User
+import com.trian.domain.models.*
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AppApiServices {
 
     @POST("/login/nurse")
     suspend fun loginNurse(): BaseResponse<List<Nurse>>
 
-    @POST("/login")
+    @POST("/login/patient")
     suspend fun loginUser(): BaseResponse<List<User>>
 
+    @POST("/register/patient")
+    suspend fun registerUser(): BaseResponse<List<User>>
+
+    @GET("/article")
+    suspend fun getArticle():BaseResponse<List<Article>>
+
+    @GET("/doctor")
+    suspend fun getDoctor():BaseResponse<List<Doctor>>
+
+    @POST("/doctor/booking")
+    suspend fun sendBooking():BaseResponse<List<Any>>
+
+    @GET("/user/orders")
+    suspend fun getListOrder():BaseResponse<List<Order>>
+
+    @GET("/user/order/show")
+    suspend fun getDetailOrder():BaseResponse<List<Any>>
+
+    @GET("/doctor/get-time-list")
+    suspend fun getListAvailableDoctorTime():BaseResponse<List<AvailableTime>>
+
+    @GET("/doctor/show-speciality")
+    suspend fun getListSpeciality():BaseResponse<List<Speciality>>
+
+    @GET("/video-conference/meeting-room")
+    suspend fun getMeetingRoom():BaseResponse<List<String>>
+
+    @GET("/record-measurement")
+    suspend fun getHealthStatus(
+        @Query("token") token:String,
+        @Query("account_number") account_number:String
+    ):BaseResponse<List<HealthStatus>>
+
+    @GET("/hospital")
+    suspend fun getListHospital():BaseResponse<List<Hospital>>
+
+    @GET("/ecommerce/product")
+    suspend fun getListProduct():BaseResponse<List<Product>>
+
+    @GET("/patient-records")
+    suspend fun getPersonalRecords():BaseResponse<List<PatientRecord>>
 
 }
--- data/src/main/java/com/trian/data/remote/app/AppRemoteDataSource.kt
+++ data/src/main/java/com/trian/data/remote/app/AppRemoteDataSource.kt
@@ -3,12 +3,39 @@ package com.trian.data.remote.app
 import com.trian.domain.repository.BaseResponse
 import com.trian.domain.entities.Nurse
 import com.trian.domain.entities.User
+import com.trian.domain.models.*
 
 
 class AppRemoteDataSource(private val apiServices: AppApiServices):IAppRemoteDataSource {
     override suspend fun loginNurse(): BaseResponse<List<Nurse>> =apiServices.loginNurse()
 
     override suspend fun loginUser(): BaseResponse<List<User>> = apiServices.loginUser()
+    override suspend fun registerUser(): BaseResponse<List<User>> = apiServices.registerUser()
+
+    override suspend fun getArticle(): BaseResponse<List<Article>> = apiServices.getArticle()
+
+    override suspend fun getDoctor(): BaseResponse<List<Doctor>> = apiServices.getDoctor()
+
+    override suspend fun sendBooking(): BaseResponse<List<Any>> = apiServices.sendBooking()
+    override suspend fun getListOrder(): BaseResponse<List<Order>> = apiServices.getListOrder()
+
+    override suspend fun getDetailOrder(): BaseResponse<List<Any>> = apiServices.getDetailOrder()
+
+    override suspend fun getListAvailableDoctorTime(): BaseResponse<List<AvailableTime>> = apiServices.getListAvailableDoctorTime()
+
+    override suspend fun getListSpeciality(): BaseResponse<List<Speciality>> = apiServices.getListSpeciality()
+
+    override suspend fun getMeetingRoom(): BaseResponse<List<String>> = apiServices.getMeetingRoom()
+    override suspend fun getHealthStatus(
+        token: String,
+        account_number: String
+    ): BaseResponse<List<HealthStatus>> =apiServices.getHealthStatus(token,account_number)
+
+    override suspend fun getListHospital(): BaseResponse<List<Hospital>> = apiServices.getListHospital()
+
+    override suspend fun getListProduct(): BaseResponse<List<Product>> = apiServices.getListProduct()
+
+    override suspend fun getPersonalRecords(): BaseResponse<List<PatientRecord>> = apiServices.getPersonalRecords()
 
 
 }
