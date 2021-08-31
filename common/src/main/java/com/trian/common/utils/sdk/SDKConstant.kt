9e2cf4351311090435e5faade7c1b4f4d20c2149
triandamai <triannurizkillah@gmail.com>
2021-08-31 13:46:04 +0700

    feat: add initialize thermometer microlife
    #151

--- common/src/main/java/com/trian/common/utils/sdk/SDKConstant.kt
+++ common/src/main/java/com/trian/common/utils/sdk/SDKConstant.kt
@@ -2,4 +2,5 @@ package com.trian.common.utils.sdk
 
 object SDKConstant {
     const val SDK_ID_BPM = "N7jbSW#j7YrFd~sE"
+    const val SDK_ID_THERMO = "1XYXF1bm8ZyEM81k"
 }
--- features/microlife/src/main/java/com/trian/microlife/di/MicrolifeModule.kt
+++ features/microlife/src/main/java/com/trian/microlife/di/MicrolifeModule.kt
@@ -3,6 +3,7 @@ package com.trian.microlife.di
 import android.app.Activity
 import android.content.Context
 import com.ideabus.model.protocol.BPMProtocol
+import com.ideabus.model.protocol.ThermoProtocol
 import com.trian.common.utils.sdk.SDKConstant
 import dagger.Module
 import dagger.Provides
@@ -17,5 +18,10 @@ object MicrolifeModule {
     @Provides
     internal fun provideBPMProtocol(@ActivityContext appContext:Context):BPMProtocol{
         return BPMProtocol.getInstance(appContext as Activity,false,false,SDKConstant.SDK_ID_BPM)
+    }
+
+    @Provides
+    internal fun provideThermoprotocol(@ActivityContext appContext: Context):ThermoProtocol{
+        return ThermoProtocol.getInstance(appContext as Activity,false,false,SDKConstant.SDK_ID_THERMO)
     }
 }
