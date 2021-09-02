f270daa5f21934a2ed16a774f92d2a6232963655
naky <rahmanecky.r@gmail.com>
2021-09-02 13:43:57 +0700

    feat (componet) : add AppBarDetail

--- component/src/main/java/com/trian/component/appbar/AppBarDetail.kt
+++ component/src/main/java/com/trian/component/appbar/AppBarDetail.kt
@@ -1,4 +1,55 @@
 package com.trian.component.appbar
 
-class AppBarDetail {
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.trian.component.ui.theme.CardColor
+import com.trian.component.ui.theme.ColorFontAppBarr
+
+@Composable
+fun AppBarDetail(page : String, onBackProggres : () -> Unit ){
+
+    TopAppBar(
+        title = {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(0.9f)
+            ) {
+
+                Text(
+                    text = page,
+                    color = ColorFontAppBarr,
+                    fontSize = 26.sp,
+                    textAlign = TextAlign.Center
+
+                    )
+            }
+        },
+        navigationIcon = {
+            Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically,modifier = Modifier.fillMaxWidth()) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.ArrowBackIos, contentDescription = "Arrow")
+                }
+
+            }
+
+        },
+
+        backgroundColor = CardColor,
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 10.dp
+    )
 }
