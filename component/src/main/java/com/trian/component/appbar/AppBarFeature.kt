06dc07235061b1fff5f8db4b0e4ca686ff6aef1a
naky <rahmanecky.r@gmail.com>
2021-09-02 13:18:08 +0700

    feat (componet) : edit AppBarFeature code

--- component/src/main/java/com/trian/component/appbar/AppBarFeature.kt
+++ component/src/main/java/com/trian/component/appbar/AppBarFeature.kt
@@ -1,7 +1,6 @@
 package com.trian.component.appbar
 
-import android.graphics.drawable.Icon
-import android.media.Image
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -9,8 +8,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.Icon
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBackIos
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,63 +20,63 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.trian.component.R
+import com.trian.component.ui.theme.CardColor
+import com.trian.component.ui.theme.ColorFontAppBarr
 
 @Composable
 fun AppBarFeature(name: String, image: String, onBackPressed: ()->Unit, onProfil:()->Unit) {
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
-            .padding(horizontal = 20.dp, vertical = 15.dp)
-            .fillMaxWidth()
-            .height(50.dp)
-    ) {
+    TopAppBar(
+        title = {
+        },
+        actions = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(end = 5.dp)
+            ) {
+                Text(
+                    text = "Hello, $name !",
+                    textAlign = TextAlign.Center,
+                    color = ColorFontAppBarr,
+                    fontSize = 16.sp
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                IconButton(onClick = { /*TODO*/ }) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
 
-        IconButton(
-            onClick = {/*TODO*/ }) {
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.example_profile),
+                            contentDescription = "profile",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(64.dp)
+                                .clip(CircleShape)                       // clip to the circle shape
+                                .border(1.dp, CardColor, CircleShape)
+                                .fillMaxHeight()
+                                .fillMaxWidth()
+                        )
 
-           Icon(Icons.Filled.ArrowBackIos, contentDescription = "Arrow")
+                    }
+                }
+            }
 
+        },
+        backgroundColor = CardColor,
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 10.dp,
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.ArrowBackIos, contentDescription = "Arrow")
+            }
         }
 
-        Row(
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            Text(
-                text = "Hello, $name !",
-                color = Color.Black,
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            //image profile
-            ImageAppBar(painter = painterResource(id = R.drawable.example_profile))
-        }
-    }
-}
-
-@Composable
-fun ImageAppBar(
-    painter: Painter,
-) {
-    
-    Box(
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(end = 5.dp)
-    ) {
-        Image(
-            painter = painter,
-            contentDescription = "profile",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(50.dp)
-                .clip(CircleShape)                       // clip to the circle shape
-                .border(1.dp, Color.Gray, CircleShape)
-        )
-
-    }
+    )
 
 }
