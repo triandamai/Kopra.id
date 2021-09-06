f7cd5f56bf6d166c7bee0bd52efa34541b12f9f9
naky <rahmanecky.r@gmail.com>
2021-09-06 18:33:44 +0700

    Feat (Component) : Add BottomSheetDevices content #84

--- features/bmi/src/main/java/com/trian/bmi/BmiActivity.kt
+++ features/bmi/src/main/java/com/trian/bmi/BmiActivity.kt
@@ -3,37 +3,14 @@ package com.trian.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBackIos
-import androidx.compose.material.icons.filled.ArrowForwardIos
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.sp
-import com.trian.component.cards.CardDetailSmartWatch
+import com.trian.component.bottomsheet.ContentBottomSheetDevices
+import com.trian.component.bottomsheet.ModalBottomSheetMainscreenView
 import com.trian.component.ui.theme.*
 
 
@@ -43,15 +20,22 @@ class BmiActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
             setContent {
                 TesMultiModuleTheme{
-                    CardDetailSmartWatch(
-                        valueHigh = "37.0",
-                        valueLow = "35.0",
-                        param = "Temperature",
-                        Satuan = "Celcius",
-                        onBackPress = {},
-                        onDatePick = {},
-                        onForwardPress = {}
+                    val modalBottomSheetState = rememberModalBottomSheetState(
+                        initialValue =ModalBottomSheetValue.Hidden
                     )
+                    val scope = rememberCoroutineScope()
+                    ModalBottomSheetLayout(
+                        sheetState = modalBottomSheetState,
+                        sheetShape = RoundedCornerShape(
+                            topStart = 15.dp,
+                            topEnd = 15.dp,
+                        ),
+                        sheetContent = {
+                            ContentBottomSheetDevices(devices = listOf(com.trian.domain.models.Devices("Microlife", "F9:CC:DD:F3:ZA", 6)))
+                        }
+                    ) {
+                        ModalBottomSheetMainscreenView(scope, modalBottomSheetState)
+                    }
                 }
             }
     }
