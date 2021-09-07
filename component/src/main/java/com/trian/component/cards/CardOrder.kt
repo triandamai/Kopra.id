83f596e6462fc068271b57c0258ea82bb3a79012
Jocerdikiawann <cocokcocok33@gmail.com>
2021-09-07 11:47:17 +0700

    feat: CardOrder.kt

--- component/src/main/java/com/trian/component/cards/CardOrder.kt
+++ component/src/main/java/com/trian/component/cards/CardOrder.kt
@@ -1,6 +1,15 @@
 package com.trian.component.cards
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun CardOrder(){}
+fun CardOrder(){
+    
+}
+
+@Preview
+@Composable
+fun PreviewCardOrder(){
+    CardOrder()
+}
