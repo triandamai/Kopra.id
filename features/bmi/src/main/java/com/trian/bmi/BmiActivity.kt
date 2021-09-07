139cbd3905be1cf1104a1f1ccaf271052f19b453
naky <rahmanecky.r@gmail.com>
2021-09-07 14:10:01 +0700

    feat (Component) : edit cardFeature #83

--- features/bmi/src/main/java/com/trian/bmi/BmiActivity.kt
+++ features/bmi/src/main/java/com/trian/bmi/BmiActivity.kt
@@ -3,41 +3,23 @@ package com.trian.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.trian.component.bottomsheet.ContentBottomSheetDevices
-import com.trian.component.bottomsheet.ModalBottomSheetMainscreenView
 import com.trian.component.cards.CardFeatures
 import com.trian.component.ui.theme.*
 
 
 class BmiActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             setContent {
 
                 TesMultiModuleTheme{
-                    val modalBottomSheetState = rememberModalBottomSheetState(
-                        initialValue =ModalBottomSheetValue.Hidden
-                    )
-                    val scope = rememberCoroutineScope()
-                    ModalBottomSheetLayout(
-                        sheetState = modalBottomSheetState,
-                        sheetShape = RoundedCornerShape(
-                            topStart = 15.dp,
-                            topEnd = 15.dp,
-                        ),
-                        sheetContent = {
-                            ContentBottomSheetDevices(devices = listOf(com.trian.domain.models.Devices("Microlife", "F9:CC:DD:F3:ZA", 6)))
-                        }
-                    ) {
-                        ModalBottomSheetMainscreenView(scope, modalBottomSheetState)
-                    }
+                    CardFeatures(image = "", name = "Body Mask Index", onClickPressed = {})
                 }
             }
     }
@@ -53,7 +35,6 @@ fun Greeting2(name: String) {
 @Composable
 fun DefaultPreview() {
     TesMultiModuleTheme {
-        CardFeatures(image = "", name ="" , onClickPressed = {})
     }
 }
 
