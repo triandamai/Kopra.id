4a1de13b7a341ee6b54f1578eb22a26a43c1bff8
Jocerdikiawann <cocokcocok33@gmail.com>
2021-09-08 16:25:02 +0700

    feat: PageLogin.kt

--- app/src/main/java/com/trian/module/ui/pages/PageLogin.kt
+++ app/src/main/java/com/trian/module/ui/pages/PageLogin.kt
@@ -1,18 +1,26 @@
 package com.trian.module.ui.pages
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.trian.common.utils.route.Routes
+import com.trian.component.ui.theme.ColorFontSw
+import com.trian.module.R
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
@@ -21,6 +29,7 @@ fun PageLogin(nav: NavHostController,scope:CoroutineScope,modifier: Modifier = M
         nav.navigate(Routes.DASHBOARD.name)
     })
 }
+
 @Composable
 fun ComponentLogin(modifier: Modifier = Modifier,onNavigate:()->Unit){
     var username by remember {mutableStateOf<String>("") }
@@ -50,10 +59,45 @@ fun ComponentLogin(modifier: Modifier = Modifier,onNavigate:()->Unit){
     }
 }
 
+@Composable
+fun PageLoginn(m:Modifier=Modifier,){
+    val emailState = remember { mutableStateOf(TextFieldValue("LOL@GMAIL.COM"))}
+    val passwordState = remember { mutableStateOf(TextFieldValue(""))}
+    ComponentTopSection()
+
+}
+
 @Preview
 @Composable
-fun PreviewComponentLogin(){
-    ComponentLogin {
-
+fun ComponentTopSection(m:Modifier=Modifier){
+    Box(
+        modifier = m
+            .height(150.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(bottomEnd = 80.dp))
+            .background(color = Color.White).shadow(
+                elevation = 2.dp
+            ),
+    ) {
+        Column(modifier = m.padding(top = 10.dp,start = 40.dp)) {
+            Image(
+                painter =
+                painterResource(
+                    id = R.drawable.logo_cexup
+                ),
+                contentDescription = "",
+                modifier = m
+                    .height(100.dp)
+                    .width(100.dp)
+            )
+            Text(text = "It's good to see you again",
+                style = MaterialTheme.typography.h1.copy(
+                    fontSize = 16.sp,
+                    letterSpacing = 1.sp,
+                    color = ColorFontSw,
+                    fontWeight = FontWeight.Medium,
+                )
+            )
+        }
     }
 }
