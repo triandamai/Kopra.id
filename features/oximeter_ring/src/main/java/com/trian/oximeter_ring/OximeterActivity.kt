ae85d4ddcf432e0cca83d092916d0d226e18246d
naky <rahmanecky.r@gmail.com>
2021-09-08 13:21:33 +0700

    feat (app) (feature) : add Spo2 ring ui #65

--- features/oximeter_ring/src/main/java/com/trian/oximeter_ring/OximeterActivity.kt
+++ features/oximeter_ring/src/main/java/com/trian/oximeter_ring/OximeterActivity.kt
@@ -8,12 +8,14 @@ import android.os.Bundle
 import android.os.IBinder
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.trian.component.ui.theme.TesMultiModuleTheme
+import com.trian.oximeter_ring.ui.OximeterUi
 import com.trian.oximeter_ring.utils.Constant
 import com.xtremeprog.sdk.ble.BleService
 
@@ -22,14 +24,13 @@ class OximeterActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
     }
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TesMultiModuleTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting4("Android")
-                }
+                OximeterUi()
             }
         }
     }
