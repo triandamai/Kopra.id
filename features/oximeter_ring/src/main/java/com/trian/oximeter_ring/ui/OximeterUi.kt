40cede00e34b24907859d0ae9593fd1025f8b121
naky <rahmanecky.r@gmail.com>
2021-09-09 15:12:26 +0700

    feat(app) (Features) : edit OximeterUi #65

--- features/oximeter_ring/src/main/java/com/trian/oximeter_ring/ui/OximeterUi.kt
+++ features/oximeter_ring/src/main/java/com/trian/oximeter_ring/ui/OximeterUi.kt
@@ -5,10 +5,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Divider
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +20,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.trian.component.R
 import com.trian.component.appbar.AppBarFeature
+import com.trian.component.cards.CardListDevice
+import com.trian.component.chart.HorizontalCircularFeatures
 import com.trian.component.ui.theme.*
 
 
@@ -43,21 +42,23 @@ fun OximeterUi(
 ){
     val spo2 :Float = 100f
     val analytic : String = "Normal"
-    Column(
-        modifier = Modifier
-            .background(LightBackground)
-            .fillMaxSize()
-            .padding(top = 10.dp, start = 10.dp, end = 10.dp)
-    ) {
-        AppBarFeature(name = "andi", image = "", onBackPressed = { /*TODO*/ }, onProfil = {})
-        Spacer(modifier = Modifier.height(10.dp))
+    Scaffold(
+        topBar = {
+            AppBarFeature(name = "andi", image = "", onBackPressed = { /*TODO*/ }, onProfil = {})
+        },
+        bottomBar = {
+            CardListDevice(status = "Device", dateStatus = "7 Days Ago" )
+        },
+        backgroundColor = LightBackground
+
+    ){
         Column(
             modifier = Modifier.verticalScroll(rememberScrollState())
         ){
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(10.dp)
+                    .padding(start = 16.dp, end = 16.dp, top = 10.dp)
                     .height(200.dp)
                     .background(color = Color.White),
                 shape = RoundedCornerShape(12.dp),
@@ -77,7 +78,7 @@ fun OximeterUi(
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        CircularValueOxi(
+                       HorizontalCircularFeatures(
                             percentage = spo2/100,
                             radius = 60.dp,
                             value = spo2.toUInt().toString(),
@@ -111,7 +112,7 @@ fun OximeterUi(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(10.dp)
+                    .padding(start = 16.dp, end = 16.dp, top = 10.dp)
                     .background(color = Color.White)
                     .height(250.dp),
                 shape = RoundedCornerShape(12.dp),
@@ -125,124 +126,8 @@ fun OximeterUi(
                 )
 
             }
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-                    .height(100.dp)
-                    .background(color = Color.White),
-                shape = RoundedCornerShape(12.dp),
-                elevation = 4.dp,
-                onClick = {/*todo*/},
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(0.3f)
-                            .fillMaxHeight()
-                            .background(Color(0xFFF395BA))
-                    ) {
 
-                    }
-                    Column(
-                        modifier = Modifier
-                            .padding(
-                                vertical = 10.dp,
-                                horizontal = 10.dp,
-                            )
-                            .fillMaxHeight(),
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            Text(
-                                text = "Device Connected",
-                                fontSize = 12.sp,
-                                color = FontDeviceConnected
-                            )
-                            Text(
-                                text = "17 day ago",
-                                fontSize = 12.sp,
-                                color = FontDeviceConnected
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(5.dp))
-                        Text(
-                            text = "Microlife A78",
-                            fontSize = 20.sp,
-                            color = FontDeviceName
-                        )
-                    }
-                }
-
-            }
         }
-
-    }
-
-}
-
-
-
-@Composable
-fun CircularValueOxi(
-    percentage: Float,
-    value: String,
-    satuan: String,
-    radius: Dp,
-    animDuration : Int = 1000,
-    animDelay: Int = 0
-){
-    var animationPlayed: Boolean by remember {
-        mutableStateOf(true)
-    }
-    val curPresentage = animateFloatAsState(
-        targetValue = if(animationPlayed) percentage else 0f,
-        animationSpec = tween(
-            durationMillis = animDuration,
-            delayMillis = animDelay
-        )
-    )
-    LaunchedEffect(key1 = true){
-        animationPlayed = true
-    }
-    Box(contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .size(radius * 2f)
-            .padding(3.dp)) {
-        Canvas(modifier = Modifier.size(radius * 2f)) {
-            drawArc(
-                color = ColorFontFeatures,
-                startAngle = -90f,
-                sweepAngle = 360 * curPresentage.value,
-                useCenter = false,
-                style = Stroke(7.dp.toPx(), cap = StrokeCap.Round)
-            )
-        }
-        Row(
-            modifier = Modifier.size(radius * 2f),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = value,
-                color = ColorFontFeatures,
-                fontSize = 24.sp,
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            Text(
-                text = satuan,
-                color = ColorFontFeatures,
-                fontSize = 16.sp,
-            )
-        }
-
     }
 
 
