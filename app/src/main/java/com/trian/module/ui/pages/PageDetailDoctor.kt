051a438a696b9bafbfd0d19d5ce126578de1f71f
Jocerdikiawann <cocokcocok33@gmail.com>
2021-09-20 15:55:34 +0700

    fix: CardOrder.kt refs #80

--- app/src/main/java/com/trian/module/ui/pages/PageDetailDoctor.kt
+++ app/src/main/java/com/trian/module/ui/pages/PageDetailDoctor.kt
@@ -158,8 +158,6 @@ private fun BodySection(m: Modifier=Modifier){
                 Spacer(modifier = m.height(5.dp))
                 Text(
                     text = "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.",
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
                     style = MaterialTheme.typography.h1.copy(
                         fontSize = 12.sp,
                         letterSpacing = 0.1.sp
