b9d5336107b636aefe27fca13d64d0617ab12e41
Jocerdikiawann <cocokcocok33@gmail.com>
2021-09-23 11:29:43 +0700

    fix: PageDetailOrder.kt refs #50

--- app/src/main/java/com/trian/module/ui/pages/PageDetailOrder.kt
+++ app/src/main/java/com/trian/module/ui/pages/PageDetailOrder.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -394,6 +396,7 @@ private fun BodySection(m: Modifier = Modifier,detailOrder: DetailOrder){
         }
     }
 }
+
 
 @Composable
 @Preview
