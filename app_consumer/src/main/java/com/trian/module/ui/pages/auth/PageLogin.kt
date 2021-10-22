d2fc934419287ee2c403d879a8f75db244c87a6b
Jocerdikiawann <cocokcocok33@gmail.com>
2021-10-22 23:05:31 +0700

    feat: PageLogin.kt refs #1

--- app_consumer/src/main/java/com/trian/module/ui/pages/auth/PageLogin.kt
+++ app_consumer/src/main/java/com/trian/module/ui/pages/auth/PageLogin.kt
@@ -1,17 +1,18 @@
 package com.trian.module.ui.pages.auth
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.accompanist.insets.navigationBarsWithImePadding
+import com.trian.component.ui.theme.BluePrimary
 import com.trian.component.utils.mediaquery.Dimensions
 import com.trian.component.utils.mediaquery.lessThan
 import com.trian.component.utils.mediaquery.mediaQuery
@@ -21,6 +22,7 @@ import compose.icons.octicons.ArrowLeft24
 
 @Composable
 fun PageLogin(m:Modifier = Modifier){
+    var numberState by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             Icon(
@@ -54,6 +56,23 @@ fun PageLogin(m:Modifier = Modifier){
                         letterSpacing = 0.1.sp,
                     )
                 )
+            )
+            Spacer(modifier = m.height(10.dp))
+            TextField(
+                value = numberState,
+                onValueChange = {numberState=it},
+                placeholder = {Text(text = "Full Name")},
+                singleLine = true,
+                modifier = m
+                    .fillMaxWidth()
+                    .navigationBarsWithImePadding(),
+                shape = RoundedCornerShape(10.dp),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = BluePrimary.copy(alpha = 0.1f),
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                ),
             )
 
         }
