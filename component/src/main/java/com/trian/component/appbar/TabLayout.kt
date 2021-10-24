e8275e062ffe63e42967c701a56d0e4037d86b08
trian damai <triannurizkillah@gmail.com>
2021-10-24 15:45:53 +0700

    feat: add list transaction
    fix #18

--- component/src/main/java/com/trian/component/appbar/TabLayout.kt
+++ component/src/main/java/com/trian/component/appbar/TabLayout.kt
@@ -10,6 +10,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.trian.component.utils.extension.customTabIndicatorOffset
+
 /**
  * Base Application
  * Author PT Cexup Telemedicine
