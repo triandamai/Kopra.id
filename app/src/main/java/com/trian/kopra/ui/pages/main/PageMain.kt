92eb8b9987ca9a064ac14427969aee4b1289a151
trian damai <triannurizkillah@gmail.com>
2021-11-04 09:55:31 +0700

    feat: add header dashboard

--- app/src/main/java/com/trian/kopra/ui/pages/main/PageMain.kt
+++ app/src/main/java/com/trian/kopra/ui/pages/main/PageMain.kt
@@ -1,12 +1,17 @@
 package com.trian.kopra.ui.pages.main
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.trian.component.cards.HeaderDashboard
 import com.trian.data.viewmodel.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 
@@ -20,11 +25,28 @@ import kotlinx.coroutines.CoroutineScope
 @Composable
 fun PageMain(
     modifier: Modifier = Modifier,
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
     mainViewModel: MainViewModel,
     navHostController: NavHostController,
     scope: CoroutineScope
 ){
+    var currentUser by mainViewModel.currentUser
+    LaunchedEffect(key1 =scaffoldState){
+        mainViewModel.getCurrentUser(){
+                success,user->
+            if(success){
+                currentUser = user
+            }
 
+        }
+    }
+    Column(
+        modifier = modifier.padding(horizontal = 16.dp)
+    ) {
+        HeaderDashboard(
+            displayName = currentUser?.fullName?:""
+        )
+    }
 }
 
 @Preview
--- component/src/main/java/com/trian/component/cards/HeaderDashboard.kt
+++ component/src/main/java/com/trian/component/cards/HeaderDashboard.kt
@@ -1,10 +1,18 @@
 package com.trian.component.cards
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 /**
  * Main Page
@@ -18,21 +26,31 @@ fun HeaderDashboard(
     modifier: Modifier=Modifier,
     displayName:String=""
 ){
-    Column {
+    Column(modifier = modifier.fillMaxWidth().background(Color.Transparent)) {
         Text(
-            text = "Halo"
+            text = "Halo",
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontWeight = FontWeight.SemiBold
+            )
         )
-        Text(text = displayName)
+        Text(
+            text = displayName,
+            style = TextStyle(
+                fontSize = 26.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
     }
 }
 
-@Preview
+@Preview(uiMode = UI_MODE_NIGHT_NO)
 @Composable
 fun PreviewHeaderDashboardLight(){
     HeaderDashboard(displayName="Trian Damai")
 }
 
-@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewHeaderDashboardDark(){
     HeaderDashboard(displayName="Trian Damai")
