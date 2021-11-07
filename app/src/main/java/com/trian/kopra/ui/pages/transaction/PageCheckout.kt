3b843dcdb7da77cbc04cd6e20f82e3934507d707
Jocerdikiawann <cocokcocok33@gmail.com>
2021-11-07 23:47:44 +0700

    fix: PageCheckout.kt refs #22

---
+++ app/src/main/java/com/trian/kopra/ui/pages/transaction/PageCheckout.kt
@@ -0,0 +1,56 @@
+package com.trian.kopra.ui.pages.transaction
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.trian.component.R
+import com.trian.component.utils.mediaquery.Dimensions
+import com.trian.component.utils.mediaquery.lessThan
+import com.trian.component.utils.mediaquery.mediaQuery
+import compose.icons.Octicons
+import compose.icons.octicons.ArrowLeft24
+
+@Composable
+fun PageCheckout(
+    modifier:Modifier=Modifier
+){
+    Scaffold (
+        topBar = {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+            ){
+                Icon(
+                    Octicons.ArrowLeft24,"",
+                )
+                Text("Checkout",style= TextStyle().mediaQuery(
+                    Dimensions.Width lessThan 400.dp, value = MaterialTheme.typography.h1.copy(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        letterSpacing = 0.1.sp
+                    )))
+                Box(){
+
+                }
+            }
+        },
+        bottomBar = {
+
+        }
+    ){
+    }
+}
